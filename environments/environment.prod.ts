4d64adee3351c9a59c47b0ad9ce52b2dff95f0eb
Outgazers <outgazers@gmail.com>
2024-08-19 19:28:27 +0330

    Update environment.prod.ts

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   version: '1.0.0',
   name: 'production',
   appName: 'Optimusgo',
-  gatewayUrl: 'https://gatewayoptimus.pharmabits.eu',
+  gatewayUrl: 'https://gateway.optimusgo.ai/',
   user_id: 'fe612cbd-9646-4b08-b2cb-bf5676b4fe2d',
 }
